0b70c19d99f7d227745a52734528f8f47088315c
AlessandroDeFa <aledefaale@gmail.com>
2023-01-25 18:42:24 +0100

    made menu page and contact page and responsive

--- src/Contact.js
+++ src/Contact.js
@@ -1,15 +1,100 @@
 import React from "react";
-import {Header, Footer } from "./index";
+import { Header, Footer } from "./index";
 
-const Contact = ()=> {
+const Contact = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    return false;
+  };
   return (
     <>
       <div className="bg-[#140e11]">
-        <Header/>
+        <Header />
       </div>
-      
+      <div className="min-h-screen bg-[hsl(350,15%,10%)] flex flex-col">
+        <div className="max-w-[95%] my-0 mx-auto pt-[15vh] w-full">
+          <div className="text-[#f8b984] text-5xl before:w-[75%] relative before:bg-[#f8b984] before:absolute before:h-[2px] before:left-[50%] before:bottom-[-0.5rem] before:translate-x-[-50%] w-fit">
+            <h1>CONTACT US</h1>
+          </div>
+          <div className="flex gap-16 pt-12 text-2xl text-white md:text-xl sm:flex-col sm:gap-8">
+            <div className="flex flex-col gap-4">
+              <p>+39 1234567891</p>
+              <p>youremail@gmail.com</p>
+            </div>
+            <div className="flex flex-col gap-4">
+              <p>32 Geylang Road,</p>
+              <p>Singapore 049400</p>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center items-center flex-auto">
+          <form
+            action=""
+            onSubmit={onSubmit}
+            className="max-w-5xl w-full mx-4 my-16"
+          >
+            <div className="flex justify-between">
+              <input
+                type="text"
+                name="name"
+                placeholder="Name *"
+                className=" text-white w-1/2 p-[.5rem] border-b-[1px] outline-none mr-4 bg-transparent"
+              />
+              <input
+                type="text"
+                name="lastname"
+                id=""
+                placeholder="Last Name *"
+                className="text-white w-1/2 p-[.5rem] border-b-[1px] outline-none bg-transparent"
+              />
+            </div>
+            <div className="flex justify-between mt-8">
+              <input
+                type="number"
+                name="phone"
+                id=""
+                placeholder="Phone Number *"
+                className="text-white w-1/2 p-[.5rem] border-b-[1px] outline-none mr-4 bg-transparent"
+              />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email Address *"
+                className="text-white w-1/2 p-[.5rem] border-b-[1px] outline-none bg-transparent"
+              />
+            </div>
+            <div className="mt-8">
+              <textarea
+                name="message"
+                id=""
+                cols="30"
+                rows="10"
+                placeholder="Your Message *"
+                className="text-white w-full p-[.5rem] border-b-[1px] outline-none bg-transparent"
+              ></textarea>
+            </div>
+            <div className="mt-8 grid grid-cols-2 sm:flex sm:flex-col">
+              <div className="text-white flex flex-row-reverse items-center sm:justify-center">
+                <label htmlFor="checkbox" className="pl-4">
+                  <p>I agree to the Cookie Policies and Privacy Policy</p>
+                </label>
+                <input type="checkbox" name="checkbox" id="checkbox" />
+              </div>
+              <div className="grid place-items-end">
+                <button
+                  type="submit"
+                  className="hover:bg-[#e4ccc0] transition duration-400  w-60 h-12 bg-[#f8b984]  text-sm text-black font-semibold uppercase md:w-52 sm:w-full sm:mt-8"
+                >
+                  SEND
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Contact;
--- src/Menu.js
+++ src/Menu.js
@@ -1,15 +1,199 @@
 import React from "react";
-import {Header, Footer } from "./index";
+import { Header, Footer } from "./index";
 
-const Menu = ()=> {
+const Menu = () => {
   return (
     <>
       <div className="bg-[#140e11]">
-        <Header/>
+        <Header />
       </div>
-      
+      <div className="min-h-screen bg-[hsl(350,15%,10%)]">
+        <div className="max-w-[95%] my-0 mx-auto w-screen flex flex-col items-center pt-8 pb-20">
+          <div className="mb-16 mt-8 before:w-[75%] relative before:bg-[#f8b984] before:absolute before:h-[2px] before:left-[50%] before:bottom-[-0.5rem] before:translate-x-[-50%]">
+            <h1 className="text-[#f8b984] text-5xl">MENU</h1>
+          </div>
+          <div className="w-full flex flex-col items-center lg:items-stretch mt-20 gap-32">
+            <div className="border-b-[1px] border-[#f8b984] before:w-full relative before:bg-[#f8b984] before:absolute before:h-[1px] before:bottom-[-0.5rem] before:left-0 pb-12">
+              <div className="text-white text-2xl text-center before:w-full relative before:bg-[#f8b984] before:absolute before:h-[1px] before:bottom-[-0.5rem] before:left-0">
+                <h2>Appetizers</h2>
+              </div>
+              <div className="flex pt-16 lg:flex-col lg:">
+                <div>
+                  <div className="text-white">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                </div>
+                <div className="ml-12 lg:ml-0 lg:pt-8">
+                  <div className="text-white">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/*----*/}
+            <div className="border-b-[1px] border-[#f8b984] before:w-full relative before:bg-[#f8b984] before:absolute before:h-[1px] before:bottom-[-0.5rem] before:left-0 pb-12">
+              <div className="text-white text-2xl text-center before:w-full relative before:bg-[#f8b984] before:absolute before:h-[1px] before:bottom-[-0.5rem] before:left-0">
+                <h2>Main Course</h2>
+              </div>
+              <div className="flex pt-16 lg:flex-col lg:">
+                <div>
+                  <div className="text-white">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                </div>
+                <div className="ml-12 lg:ml-0 lg:pt-8">
+                  <div className="text-white">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            {/*----*/}
+            <div className="border-b-[1px] border-[#f8b984] before:w-full relative before:bg-[#f8b984] before:absolute before:h-[1px] before:bottom-[-0.5rem] before:left-0 pb-12">
+              <div className="text-white text-2xl text-center before:w-full relative before:bg-[#f8b984] before:absolute before:h-[1px] before:bottom-[-0.5rem] before:left-0">
+                <h2>Drinks</h2>
+              </div>
+              <div className="flex pt-16 lg:flex-col lg:">
+                <div>
+                  <div className="text-white">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                </div>
+                <div className="ml-12 lg:ml-0 lg:pt-8">
+                  <div className="text-white">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                  <div className="text-white pt-12">
+                    <div className="flex justify-between">
+                      <h2 className="text-xl">blabla</h2>
+                      <span className="text-[#f8b984]">€ 12,00</span>
+                    </div>
+                    <p className="pt-4 lg:text-sm">
+                      Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                      Esse, cum!
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
-}
+  );
+};
 
 export default Menu;
